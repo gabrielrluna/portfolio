402ecc213849fc7b50e493598558a340bdb362d3
Gabriel Luna <lunagabri@gmail.com>
2025-05-13 20:03:13 -0300

    Definição do Carrossel

--- js/script.js
+++ js/script.js
@@ -1,54 +1,76 @@
-// Script do Swiper 
+const wrapper = document.querySelector(".wrapper");
+const carousel = document.querySelector(".carousel");
+const arrowBtns = document.querySelectorAll(".wrapper i");
+const firstCardWidth = carousel.querySelector(".card").offsetWidth;
+const carouselChildrens = [...carousel.children];
 
-// const swiper = new Swiper('.swiper', {
-//     loop: true,
-//     grabCursor: true,
-//     spaceBetween: 30,
-  
-//     // If we need pagination
-//     pagination: {
-//       el: '.swiper-pagination',
-//       clickable: true,
-//       dynamicBullets: true
-//     },
-  
-//     // Navigation arrows
-//     navigation: {
-//       nextEl: '.swiper-button-next',
-//       prevEl: '.swiper-button-prev',
-//     },
+let isDragging = false, startX, startScrollLeft, timeoutId;
 
-//     breakpoints: {
-//         0: {
-//             slidesPerView: 1
-//         },
-//         620: {
-//             slidesPerView: 2
-//         },
-//         1024: {
-//             slidesPerView: 3 
-//         }
+let cardPerView = Math.round(carousel.offsetWidth / firstCardWidth);
 
-//     }
-//   });
+carouselChildrens.slice(-cardPerView).reverse().forEach(card => {
+  carousel.insertAdjacentHTML("afterBegin", card.outerHTML);
+});
 
-  const swiper = new Swiper('.swiper', {
+carouselChildrens.slice(0, cardPerView).forEach(card => {
+  carousel.insertAdjacentHTML("beforeend", card.outerHTML);
+});
 
-    loop: true,
-  
-    // If we need pagination
-    pagination: {
-      el: '.swiper-pagination',
-    },
-  
-    // Navigation arrows
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
-    },
-  
-    // And if we need scrollbar
-    scrollbar: {
-      el: '.swiper-scrollbar',
-    },
-  });
+arrowBtns.forEach(btn => {
+  btn.addEventListener("click", () => {
+    // console.log(btn.id);
+    carousel.scrollLeft += btn.id === "left" ? -firstCardWidth : firstCardWidth;
+  })
+})
+
+
+//As funções a seguir servem para que o carrossel consiga ser mexido com o arrastar do mouse.
+const dragStart = (e) => {
+  isDragging = true;
+  carousel.classList.add("dragging");
+  startX = e.pageX;
+  startScrollLeft = carousel.scrollLeft;
+}
+
+const dragging = (e) => {
+  // console.log(e.pageX);
+  if(!isDragging) return;
+  carousel.scrollLeft = startScrollLeft - (e.pageX - startX);
+}
+
+const dragStop = () => {
+  isDragging = false;
+  carousel.classList.remove("dragging");
+}
+
+
+//Função que ativa o rolamento automático dos cards
+const autoPlay = () => {
+  if(window.innerWidth < 800) return;
+  timeoutId = setTimeout(() => carousel.scrollLeft += firstCardWidth, 3500);
+}
+
+autoPlay(); //Aqui o rolamento automático é ativado. 
+
+//O script abaixo deixa o carrossel "infinito"
+const infiniteScroll = () => {
+  if(carousel.scrollLeft === 0) {
+    carousel.classList.add("no-transition");
+    carousel.scrollLeft = carousel.scrollWidth - (2 * carousel.offsetWidth);
+    carousel.classList.remove("no-transition");
+  } else if(Math.ceil(carousel.scrollLeft) === carousel.scrollWidth - carousel.offsetWidth){
+    carousel.classList.add("no-transition");
+    carousel.scrollLeft = carousel.offsetWidth;
+    carousel.classList.remove("no-transition");
+
+  }
+  clearTimeout(timeoutId);
+  if(!wrapper.matches(":hover")) autoPlay();
+}
+
+carousel.addEventListener("mousedown", dragStart);
+carousel.addEventListener("mousemove", dragging);
+document.addEventListener("mouseup", dragStop);
+wrapper.addEventListener("mouseenter", () => clearTimeout(timeoutId));
+wrapper.addEventListener("mouseleave", autoPlay);
+carousel.addEventListener("scroll", infiniteScroll);
